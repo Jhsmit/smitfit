9a3c3c520fab07819d92863c02d2a33c01736731
Jochem Smit <jhsmit@gmail.com>
2025-02-21 14:06:46 +0100

    fix test

--- tests/test_other.py
+++ tests/test_other.py
@@ -6,6 +6,7 @@ def test_model_from_str():
     model = Model("y == a * x + b")
     assert model.y_symbols == {sp.Symbol("y")}
     assert model.x_symbols == {sp.Symbol("x"), sp.Symbol("a"), sp.Symbol("b")}
-    assert model.model[sp.Symbol("y")] == sp.Add(
-        sp.Mul(sp.Symbol("a"), sp.Symbol("x")), sp.Symbol("b")
-    )
+
+    lhs = model.model[sp.Symbol("y")]
+    rhs = sp.Add(sp.Mul(sp.Symbol("a"), sp.Symbol("x")), sp.Symbol("b"))
+    assert sp.Equality(lhs, rhs)
