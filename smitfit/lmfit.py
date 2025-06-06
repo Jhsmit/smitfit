953bf7eba706de3107517580ea9c1cb2602afbf0
Jochem Smit <jhsmit@gmail.com>
2025-06-06 12:48:24 +0200

    maximul likelihood fitting and errors

--- smitfit/lmfit.py
+++ smitfit/lmfit.py
@@ -1,11 +1,68 @@
 import lmfit as lm
 import numpy as np
 
-from smitfit.loss import SELoss
+from smitfit.loss import SELoss, Loss
 from smitfit.model import Model
 from smitfit.parameter import Parameters
 from smitfit.result import Result
 from smitfit.utils import flat_concat
+
+
+# WIP class to replace Minimize
+# this Minimize also supports scalar loss functions instead of requiring residuals
+class NewMinimize:
+    def __init__(
+        self,
+        loss: Loss,
+        parameters: Parameters,
+        xdata: dict[str, np.ndarray],
+    ):
+        self.loss = loss
+        self.parameters = parameters
+        self.xdata = xdata
+
+    def fit(self, residuals=True, method: str | None = None):
+        lm_params = lm.Parameters()
+        for par in self.parameters:
+            lm_params.add(
+                par.symbol.name,
+                value=par.guess,
+                min=par.bounds[0],
+                max=par.bounds[1],
+                vary=not par.fixed,
+            )
+
+        def residual(param):
+            return flat_concat(self.loss.residuals(**self.xdata, **param.valuesdict()))
+
+        def loss(param):
+            return float(self.loss(**self.xdata, **param.valuesdict()))
+
+        minfunc = residual if residuals else loss
+
+        if method is None and residuals:
+            method = "leastsq"
+        elif method is None:
+            method = "lbfgsb"
+
+        result = lm.minimize(minfunc, lm_params, method=method)
+        fit_parameters = {k.name: result.params[k.name].value for k in self.parameters.free}
+
+        # TODO redchi, aic, bic, r_squared
+        gof_qualifiers = {"chisqr": result.chisqr}
+        if result.errorbars:
+            errors = {k.name: result.uvars[k.name].std_dev for k in self.parameters.free}
+        else:
+            errors = {}
+
+        return Result(
+            fit_parameters=fit_parameters,
+            gof_qualifiers=gof_qualifiers,
+            errors=errors,
+            fixed_parameters=self.parameters.fixed.guess,
+            guess=self.parameters.free.guess,
+            base_result=result,
+        )
 
 
 class Minimize:
--- smitfit/loss.py
+++ smitfit/loss.py
@@ -3,6 +3,8 @@ from typing import Optional
 from smitfit.model import Model
 from smitfit.reduce import mean_reduction, sum_reduction
 
+import numpy as np
+
 
 class Loss:
     """sum/average reduction"""
@@ -14,6 +16,7 @@ class Loss:
 class SELoss(Loss):
     """sum of squared errors"""
 
+    # todo super call pass model
     def __init__(self, model: Model, y_data: dict, weights: Optional[dict] = None):
         self.model = model
         self.y_data = y_data
@@ -45,3 +48,17 @@ class MSELoss(SELoss):
         squares = self.squares(**kwargs)
 
         return mean_reduction(squares)
+
+
+class NLLLoss(Loss):
+    """Negative Log Likelihood Loss"""
+
+    def __init__(self, model: Model, weights: Optional[dict] = None):
+        self.model = model
+        self.weights = weights or {}
+
+    def __call__(self, **kwargs) -> float:
+        y_model = self.model(**kwargs)
+
+        log_likelihoods = {k: -self.weights.get(k, 1) * np.log(y_model[k]) for k in y_model}
+        return sum_reduction(log_likelihoods)
